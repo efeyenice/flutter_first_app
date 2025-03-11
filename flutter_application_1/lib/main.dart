71cd449694b171b7d4117dc8de3f92e3c2522e32
Efe Yenice <efeyenice004@gmail.com>
2025-03-11 18:18:58 +0300

    created like button and funcionalty

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -32,6 +32,19 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+
+
 }
 
 class MyHomePage extends StatelessWidget {
@@ -49,13 +62,25 @@ class MyHomePage extends StatelessWidget {
           children: [
             BigCard(pair: pair),
             SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text("Next"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(appState.favorites.contains(pair) ? Icons.favorite : Icons.favorite_border),
+                  label: Text("Like"),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text("Next"),
+                ),
+              ],
             ),
-        
           ],
         ),
       ),
